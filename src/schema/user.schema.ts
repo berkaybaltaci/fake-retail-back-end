08ef07655ea1ad85f8d9306cacabe20460986fe4
berkaybaltaci <berkaybaltaci@gmail.com>
2022-08-04 18:33:38 +0300

    Added constraints for register inputs

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,6 +1,7 @@
 import { getModelForClass, pre, prop } from '@typegoose/typegoose';
 import { Field, InputType, ObjectType } from 'type-graphql';
 import bcrypt from 'bcrypt';
+import { MaxLength, MinLength, NotContains } from 'class-validator';
 
 @pre<User>('save', async function () {
   // Check that the password is being modified
@@ -36,10 +37,16 @@ export class GetUserByIdInput {
 
 @InputType()
 export class CreateUserInput {
+  @MinLength(3, { message: 'Username should be at least 3 characters.' })
+  @MaxLength(15, { message: 'Username should be at most 15 characters.' })
+  @NotContains(' ', { message: 'Username cannot contain empty space.' })
   @Field()
   name: string;
 
   @Field()
+  @NotContains(' ', { message: 'Password cannot contain empty space.' })
+  @MinLength(6, { message: 'Password should be at least 6 characters.' })
+  @MaxLength(15, { message: 'Password should be at most 15 characters.' })
   password: string;
 }
 
